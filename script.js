86aec94ea38ee991266b348ba267bcf8955859a1
yuss22 <yousrachebbah22@gmail.com>
2025-02-18 19:53:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Exemple de script pour afficher une alerte quand on clique sur "Contact"
+document.querySelector('footer').addEventListener('click', function() {
+    alert("Merci de nous contacter !");
+});
