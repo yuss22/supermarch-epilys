dcb07fbd8f111061324655989670718879a37308
yuss22 <yousrachebbah22@gmail.com>
2025-02-18 20:15:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-// Exemple de script pour afficher une alerte quand on clique sur "Contact"
-document.querySelector('footer').addEventListener('click', function() {
-    alert("Merci de nous contacter !");
-});
+let currentIndex = 0;
+const images = document.querySelectorAll('#banniere .slider img');
+const totalImages = images.length;
+
+function changeImage() {
+    images[currentIndex].style.display = 'none';
+    currentIndex = (currentIndex + 1) % totalImages;
+    images[currentIndex].style.display = 'block';
+}
+
+setInterval(changeImage, 3000); // Change d'image toutes les 3 secondes
